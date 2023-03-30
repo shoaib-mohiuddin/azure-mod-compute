421aa0dff296016109734894506a9304b90ecc8e
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2023-03-30 20:33:46 +0530

    Terratest

--- test/module_test.go
+++ test/module_test.go
@@ -51,5 +51,5 @@ func Test_VM(t *testing.T) {
 
         // Make an HTTP request to the instance and make sure we get back a 200 OK with the body "Hello, World!"
         url := fmt.Sprintf("http://%s:80", publicIp)
-        http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 10, 3*time.Second)
+        http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 20, 5*time.Second)
 }
